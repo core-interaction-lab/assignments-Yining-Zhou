ed62638bd28547e3981e3579c2a79c4cc598cbc9
Yining Zhou <zhouy142@newschool.edu>
2022-05-06 10:03:52 -0400

    1

--- assignments/assignment-5/js/main.js
+++ assignments/assignment-5/js/main.js
@@ -1,53 +1,50 @@
-const todoTextarea = document.getElementById('todo-textarea');
-const addTodoBtn = document.getElementById('add-todo-btn');
-const todoContainer = document.getElementById('todo-container');
+const todoTextarea = document.querySelectorAll(".todo-textarea");
+const resultAnswers = document.querySelectorAll(".result-answer");
+const addTodoBtn = document.getElementById("add-todo-btn");
+const todoContainer = document.getElementById("todo-container");
 
 const state = {
-    todoItems: [],
+  todoItems: [],
 };
 
-const setTodoItems = items => {
-    state.todoItems = items;
+const setTodoItems = (items) => {
+  state.todoItems = items;
 };
 
-const addTodoItem = item => {
-    const todoItemCopy = state.todoItems.slice();
-    todoItemCopy.push(item);
-    setTodoItems(todoItemCopy);
+const addTodoItem = (item) => {
+  const todoItemCopy = state.todoItems.slice();
+  todoItemCopy.push(item);
+  setTodoItems(todoItemCopy);
 };
 
-const removeTodoItem = index => {
-    const todoItemCopy = state.todoItems.slice();
-    todoItemCopy.splice(index,1);
-    setTodoItems(todoItemCopy);
-
-}
-
-const buildTodoItem = (item,index) =>{
-    const todoEl = document.createElement('article');
-    const textEl = document.createElement('p');
-    const deleteBtn = document.createElement ('button');
-
-    deleteBtn.innerHTML = 'delete';
-
-    textEl.innerHTML = item;
-    
+const removeTodoItem = (index) => {
+  const todoItemCopy = state.todoItems.slice();
+  todoItemCopy.splice(index, 1);
+  setTodoItems(todoItemCopy);
 };
 
-const buildTodoItems = items => {
-    todoContainer.innerHTML = '';
-    const todoItemEls = items.map(buildTodoItem);
-    todoContainer.append(...todoItemEls);
-}
+const buildTodoItem = (item, index) => {
+  const todoEl = document.createElement("article");
+  const textEl = document.createElement("p");
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.innerHTML = "delete";
+
+  textEl.innerHTML = item;
+};
+
+const buildTodoItems = (items) => {
+  todoContainer.innerHTML = "";
+  const todoItemEls = items.map(buildTodoItem);
+  todoContainer.append(...todoItemEls);
+};
 
 const main = () => {
-    addTodoBtn.addEventListener('click',evt => {
-        const todoValue = todoTextarea.value;
-        if (todoValue.length > 0){
-            addTodoItem(todoValue);
-            buildTodoItems(state.todoItems);
-        }
+  addTodoBtn.addEventListener("click", (evt) => {
+    resultAnswers.forEach((el, i) => {
+      el.textContent = todoTextarea[i].value;
     });
+  });
 };
 
 main();
